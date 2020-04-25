c4d8f8700c84d5413ef5e4cf578da299477bb319
Hussain101 <64015627+Hussain101@users.noreply.github.com>
2020-04-25 02:27:16 -0700

    Add files via upload
    To make a calculator works with 10 function

--- main.cpp
+++ main.cpp
@@ -4,24 +4,75 @@ using namespace std;
 
 int main()
 {
-    int n ;
-    double answer ,guess,previous ,r;
-    cout << "This program will calculate the square root of the number \n using the babaybolian algorithm." << endl ;
-    cout << "Enter a whole  number="
-;
-    cin >> n ;
-    r=n/guess;
-    guess=(guess+r)/2;
-    previous+=guess;
-    while(n>guess);
+    int A,B ;
+    char choice;
+    double answer;
+    cout << "This calculator tales two input from you and work according to your given operation! " << endl;
+    cout << "Enter two numbers\n";
+    cout << "A=" ;
+    cin >> A ;
+    cout << "B=" ;
+    cin >> B ;
+    cout << endl ;
+    cout << "Choose your operation from following!\n" ;
+    cout << "OP0      Addition\n" ;
+    cout << "OP1      Subtraction\n" ;
+    cout << "OP2      Product\n" ;
+    cout << "OP3      Division\n" ;
+    cout << "OP4      Remainder\n" ;
+    cout << "OP5      Sq of A\n" ;
+    cout << "OP6      Sq of B\n" ;
+    cout << "OP7      Inccrement of A\n" ;
+    cout << "OP8      Decrement of A\n" ;
+    cout << "OP9      Increment of B\n" ;
+    cout << endl ;
+    cout << "Enter operation number=" ;
+    cin >> choice ;
+    switch(choice)
     {
-        if(previous<=(guess*0.01)+guess)
-            answer+=previous;
-        else
-            r=n/guess;
-    guess=(guess+r)/2;
-    previous+=guess;
+    case '0' :
+        answer = A+B ;
+        cout << "A+B=" << answer << endl ;
+        break;
+
+    case '1' :
+        answer = A-B ;
+        cout << "A-B=" << answer << endl ;
+        break;
+    case '2' :
+        answer = A*B ;
+        cout << "A*B=" << answer << endl ;
+        break;
+    case '3' :
+        answer = A/B ;
+        cout << "A/B=" << answer << endl ;
+        break;
+    case '4' :
+        answer = A%B ;
+        cout << "A%B=" << answer << endl ;
+        break;
+    case '5' :
+        answer = A*A ;
+        cout << "A*A=" << answer << endl ;
+        break;
+    case '6' :
+        answer = B*B ;
+        cout << "B*B=" << answer << endl ;
+        break;
+    case '7' :
+        answer = A++ ;
+        cout << "A increment=" << answer << endl ;
+        break;
+    case '8' :
+        answer = A-- ;
+        cout << "A decrement=" << answer << endl ;
+        break;
+     case '9' :
+        answer = B-- ;
+        cout << "B decrement=" << answer << endl ;
+        break;
+     default :
+        cout << "Not a vslid operation" ;
     }
-    cout << "The square root of_" << n << "is_" << answer ;
     return 0;
 }
