b2cce6bfd77bbd530ff84e3b1f119ba7ea09b9dc
Hussain101 <64015627+Hussain101@users.noreply.github.com>
2020-04-25 02:46:08 -0700

    Add files via upload
    Notification program

--- main.cpp
+++ main.cpp
@@ -4,17 +4,27 @@ using namespace std;
 
 int main()
 {
-    int x ,y, z;
-    cout << "This program show the multiplication table of number of its multiple" << endl;
-    cout << "Enter the number-" ;
-    cin >> x ;
-    cout << "Enter the value of y=" ;
-    cin >> y ;
-    cout << endl ;
-        for(z=1 ; z<=y; z++)
+    string day ;
+    int timeh, timem , length;
+    char nothing ;
+    double cost;
+    cout << "Enter the day as sa for Saturday :" ;
+    cin >> day ;
+    cout << "Enter the time the call started in 24-hour notification as (13:30) for 01:30  " ;
+    cin >> timeh >> nothing >> timem ;
+    cout << "Enter the call length minutes as rounded off to whole numbers :" ;
+    cin >> length ;
+    if((day != "SA" )||(day!="sa")||(day!="Sa")||(day!="sA")||(day!="su")||(day!="sU")||(day!="SU")||(day!="Su"))
     {
-      cout << x << '*' << z << "=" << x*z << "\n" ;
-    }
-
+    	if ((8 <=timeh)||(18 <=timeh))
+    	cost = 0.4 ;
+    	else
+    	cost = 0.25 ;
+	}
+	else 
+	cost =0.15 ;
+	cout << endl ;
+	cout << "THe cost of the call is $" << cost*length ;
+	cout << "At the rate of $" << cost << "per minute .\n" ;
     return 0;
 }
